96d164f42c033398f39c08eafc43aacdd1ccba6f
Ryan Berg <ryan.berg@gmail.com>
2009-09-08 21:25:08 -0400

    context_processor for ADMINLINKS_MEDIA_URL now has a default url using settings.MEDIA_URL

--- adminlinks/context_processors.py
+++ adminlinks/context_processors.py
@@ -2,6 +2,27 @@ from django.conf import settings
 
 def media(request):
 	"""
-	Returns the URL of adminlinks media
+	Returns the URL of adminlinks media specified in settings
+	Defaults to the standard MEDIA_URL setting + '/adminlinks/'
 	"""
-	return {'ADMINLINKS_MEDIA_URL': settings.ADMINLINKS_MEDIA_URL}
+	
+	try:
+		ADMINLINKS_MEDIA_URL = settings.ADMINLINKS_MEDIA_URL
+	
+	except AttributeError:
+		ADMINLINKS_MEDIA_URL = ''
+		
+		# Only need to get MEDIA_URL if ADMINLINKS_MEDIA_URL is not specified
+		try:
+			MEDIA_URL = settings.MEDIA_URL
+			
+			# Make sure MEDIA_URL ends in a '/'
+			if MEDIA_URL[-1] != '/':
+				MEDIA_URL += '/'
+				
+			ADMINLINKS_MEDIA_URL = '%sadminlinks/' % (MEDIA_URL)
+			
+		except AttributeError:
+			MEDIA_URL = ''
+			
+	return {'ADMINLINKS_MEDIA_URL': ADMINLINKS_MEDIA_URL}
